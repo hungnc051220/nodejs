770d84f8be1c1c9bca6ba48fa2e4a559a1f9ce57
ADMIN-PC\Admin <hungnc051220@gmail.com>
2022-02-02 12:02:28 +0700

    Lab 1.3

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const http = require('http');
 
-const server = http.createServer((res, req) => {
-    console.log(req);
+const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'text/html')
+
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><h1>Hello from my Node.js server!</h1></body>');
+    res.write('</html>');
+    
+    res.end();
 })
 
 server.listen(3000);
