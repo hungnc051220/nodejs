9bbdbcc4ea63482a6448ceb274fe544abbad4f48
ADMIN-PC\Admin <hungnc051220@gmail.com>
2022-02-02 20:15:31 +0700

    Lab 1.6

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const http = require("http");
-const fs = require('fs')
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   const url = req.url;
@@ -7,17 +7,31 @@ const server = http.createServer((req, res) => {
   if (url === "/") {
     res.write("<html>");
     res.write("<head><title>Enter message</title></head>");
-    res.write("<body><form action='/message' method='POST'><input type='text'><button type='submit'>Send</button></form></body>");
+    res.write(
+      "<body><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form></body>"
+    );
     res.write("</html>");
 
     return res.end();
   }
 
-  if(url === '/message' && method === 'POST'){
-    fs.writeFileSync('message.txt', 'DUMMY');
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on('data', (chunk) => {
+        console.log(chunk);
+        body.push(chunk);
+    })
+
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      console.log(parsedBody);
+      const message = parsedBody.split('=')[1];
+      fs.writeFileSync("message.txt", message);
+    });
+
     res.statusCode = 302;
-    res.setHeader('Location', '/');
-    
+    res.setHeader("Location", "/");
+
     return res.end();
   }
 
