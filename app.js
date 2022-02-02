bdcbcd5044fd2be47ea4b7d19c551bd32fd25e6e
ADMIN-PC\Admin <hungnc051220@gmail.com>
2022-02-02 20:53:31 +0700

    Lab 2.4

--- app.js
+++ app.js
@@ -4,13 +4,17 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("In the middleware");
+app.use("/", (req, res, next) => {
+  console.log('This alway runs');
   next();
 });
 
-app.use((req, res, next) => {
-  res.send('<h1>Hello from Express!</h1>')
+app.use("/add-product", (req, res, next) => {
+  res.send("<h1>The Add product page</h1>");
+});
+
+app.use("/", (req, res, next) => {
+  res.send("<h1>Hello from Express!</h1>");
 });
 
 const server = http.createServer(app);
