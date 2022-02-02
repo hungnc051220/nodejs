6f7a43bc817903ae1a1838243e9fdaaebf8d9443
ADMIN-PC\Admin <hungnc051220@gmail.com>
2022-02-02 21:42:32 +0700

    Lab 2.12

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const bodyParser = require("body-parser");
 
 const app = express();
@@ -8,10 +9,10 @@ const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/admin', adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page Not Found</h1>");
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 app.listen(3000);
