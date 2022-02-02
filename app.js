ede3a0d52a64574867fe8413295538d300e819db
ADMIN-PC\Admin <hungnc051220@gmail.com>
2022-02-02 12:08:35 +0700

    Lab 1.4

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-const http = require('http');
+const http = require("http");
 
 const server = http.createServer((req, res) => {
-    res.setHeader('Content-Type', 'text/html')
+  const url = req.url;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter message</title></head>");
+    res.write("<body><form action='/message' method='POST'><input type='text'><button type='submit'>Send</button></form></body>");
+    res.write("</html>");
 
-    res.write('<html>');
-    res.write('<head><title>My First Page</title></head>');
-    res.write('<body><h1>Hello from my Node.js server!</h1></body>');
-    res.write('</html>');
-    
-    res.end();
-})
+    return res.end();
+  }
+  res.setHeader("Content-Type", "text/html");
+
+  res.write("<html>");
+  res.write("<head><title>My First Page</title></head>");
+  res.write("<body><h1>Hello from my Node.js server!</h1></body>");
+  res.write("</html>");
+
+  res.end();
+});
 
 server.listen(3000);
